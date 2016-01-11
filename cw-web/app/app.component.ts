339a31a8d6ae252aa953d8d95595a614daf81477
Grębowiec Dariusz <dariusz.grebowiec@coi.gov.pl>
2016-01-11 16:19:29 +0100

    angular test

---
+++ cw-web/app/app.component.ts
@@ -0,0 +1,14 @@
+import {Component} from 'angular2/core'
+
+@Component({
+    selector: 'my-app',
+    template: '
+        <h1>test </h1>
+      <div> <label> {{dupa}} </label></div>
+       <div> <input [(ngModel)]="dupa" placeholder="rower"> </div>
+    '
+
+})
+export class AppComponent {
+    dupa = 'dupa100';
+}
