570342f2d20f704f535d0353587697c4d09098a3
dgrebowiec <dgrebowiec@nexio.pl>
2016-01-15 20:34:44 +0100

    poprawa postrofow - multi-line

--- cw-web/app/app.component.ts
+++ cw-web/app/app.component.ts
@@ -1,14 +1,22 @@
-import {Component} from 'angular2/core'
+import {Component} from 'angular2/core';
+import {Http} from 'angular2/http';
+//import {Injectable} from 'angular2/core';
 
 @Component({
     selector: 'my-app',
+    //viewProviders: [HTTP_PROVIDERS],
     template: `
         <h1>test </h1>
-      <div> <label> {{dupa}} </label></div>
-       <div> <input [(ngModel)]="dupa" placeholder="rower"> </div>
+        <div> <label> {{dupa}} </label></div>
+        <div> <input [(ngModel)]="dupa" placeholder="rower"> </div>
     `
-
 })
+
+//@Injectable()
 export class AppComponent {
+   /* constructor(http: Http) {
+        //http.get('/words')
+
+    }*/
     dupa = 'dupa100';
 }
