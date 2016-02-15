f6fc32d79c059c28f0e64c77c01233d76f7f3c17
dgrebowiec <dgrebowiec@nexio.pl>
2016-02-15 17:47:22 +0100

    grunt

--- frontend/Gruntfile.js
+++ frontend/Gruntfile.js
@@ -19,10 +19,18 @@ module.exports = function (grunt) {
                     filter: 'isFile'
                 }, {expand: true, src: 'index.html', dest: 'build/', flatten: true, filter: 'isFile'}]
             }
+        },
+        connect: {
+            server: {
+                options: {
+                    port: 8088
+                }
+            }
         }
     });
     grunt.loadNpmTasks('grunt-typescript');
+    grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.registerTask("default", ["typescript"]);
-    grunt.registerTask("build", ["typescript"]);
+    grunt.registerTask("default", ['connect:server', 'typescript']);
+    grunt.registerTask("build", ['connect:server', 'typescript']);
 };
